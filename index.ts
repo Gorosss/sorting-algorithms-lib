57a006ee946a998198ea3fad615aadec540f90d3
Gorosss <jongorostegui@gmail.com>
2025-02-24 02:40:38 +0100

    case 1

---
+++ index.ts
@@ -0,0 +1,56 @@
+/**
+ * Given an array, swaps two elements at the specified positions (indices).
+ * @param array Array in which to swap elements between two positions
+ * @param pos1 Index of first element's position
+ * @param pos2 Index of second element's position
+ */
+function swap(array: Array<any>, pos1: number, pos2: number) {
+  let temp = array[pos1];
+  array[pos1] = array[pos2];
+  array[pos2] = temp;
+}
+
+function partition(
+  array: Array<any>,
+  pivotIndex: number,
+  leftBound: number,
+  rightBound: number
+) {
+  let pivot = array[pivotIndex];
+  let partitionIndex = leftBound;
+
+  for (let i = leftBound; i < rightBound; ++i) {
+    if (array[i] < pivot) {
+      swap(array, i, partitionIndex);
+      ++partitionIndex;
+    }
+  }
+  swap(array, rightBound, partitionIndex);
+  return partitionIndex;
+}
+
+/**
+ * Sort an array of numbers or strings using Quick Sort
+ * @param array Complete array to be sorted
+ * @param leftBound Index of left-most element for section to be sorted
+ * @param rightBound Index of right-most element for section to be sorted
+ * @returns Returns sorted array
+ */
+function quickSort(
+  array: number[] | string[],
+  leftBound: number,
+  rightBound: number
+): Array<any> {
+  let pivotIndex: number;
+  let partitionIndex: number;
+
+  if (leftBound < rightBound) {
+    pivotIndex = rightBound;
+    partitionIndex = partition(array, pivotIndex, leftBound, rightBound);
+
+    // Sort left & right sub-sections
+    quickSort(array, leftBound, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, rightBound);
+  }
+  return array;
+}
